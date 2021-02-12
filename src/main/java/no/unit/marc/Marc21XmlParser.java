1b2c9e27e1fd5a779dcda65296b05afc6cf4ab18
Eirik Eide Saur <eisa@unit.no>
2021-02-12 11:47:42 +0100

    Trying to remove group from gradle.build

--- src/main/java/no/unit/marc/Marc21XmlParser.java
+++ src/main/java/no/unit/marc/Marc21XmlParser.java
@@ -64,7 +64,7 @@ public class Marc21XmlParser {
     }
 
     public String versionCheck(){
-        return "Third verison";
+        return "Fourth verison";
     }
 
     private void checkFirstTag(Document xmlDoc) throws Marc21XmlParserException {
